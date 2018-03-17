683d6bb7d7772b1ec7500ef8bd1bddc3bf96343c
mauryahdx <suryamourya@iiitdmj.ac.in>
2018-03-17 23:28:47 +0530

    added restauranrts

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from restaurants.views import home
+from django.views.generic import TemplateView
+from restaurants.views import restaurant_listview
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^restaurants/', restaurant_listview),
+    url(r'^home/', TemplateView.as_view(template_name="home.html")),
+    url(r'^about/', TemplateView.as_view(template_name="about.html")),
+    url(r'^contact/', TemplateView.as_view(template_name="contact.html")),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView
+from .models import RestaurantLocation
 
-# Create your views here.
-def home(request):
-	return HttpResponse("YOYO")
+def restaurant_listview(request):
+	template_name = 'restaurants/restaurants_list.html'
+	queryset = RestaurantLocation.objects.all()
+	context = {
+				"objectList":queryset,
+				}
+	return render(request, template_name, context)
+	
+
