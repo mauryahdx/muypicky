54939b813a8714cf3c80f4936a8afcb1f11adbd3
mauryahdx <suryamourya@iiitdmj.ac.in>
2018-03-18 06:18:26 +0530

    restaurant locations added

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,7 +1,8 @@
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import RestaurantLocation
 
 def restaurant_listview(request):
@@ -11,5 +12,29 @@ def restaurant_listview(request):
 				"objectList":queryset,
 				}
 	return render(request, template_name, context)
-	
+
+class RestaurantListView(ListView):
+	context_object_name = 'objectList'
+	template_name = 'restaurants/restaurants_list.html'
+	def get_queryset(self):
+		slug = self.kwargs.get("slug")
+		if slug:
+			queryset = RestaurantLocation.objects.filter(Q(category__iexact = slug)|Q(category__icontains = slug))
+		else:
+			queryset = RestaurantLocation.objects.all()	
+		return queryset	
+
+class RestaurantDetailView(DetailView):
+	queryset = RestaurantLocation.objects.all()
+	context_object_name = 'obj'
+	#def get_context_data(self, *args, **kwargs):
+	#	print(self.kwargs)
+	#	context = super(RestaurantDetailView, self).get_context_data(*args, **kwargs)
+	#	print(context)
+	#	return context
+
+#class AmericanRestaurantListView(ListView):
+#	queryset = RestaurantLocation.objects.filter(category__iexact='american')	
+#	template_name = 'restaurants/restaurants_list.html'	
+
 
